39fb92d7b00c42e407759235a1fba7134b8f6df5
David Škudar <dskudar20@foi.hr>
2024-11-18 14:18:08 +0100

    SCRUM-53 Created .gitignore for api project.

--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c63473512598c726271168a3705ebcc317a2c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73efe68ef643b64d49b508479ae4c5e70e29c24f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
