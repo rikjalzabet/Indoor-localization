1543b3fca16186a860e6227bfe9113c7e4d52984
ahamzic20 <ahamzic20@foi.hr>
2024-11-20 18:19:01 +0100

    SCRUM-171 created table of assets with material

--- web-ui/src/app/asset-management/asset-management.component.ts
+++ web-ui/src/app/asset-management/asset-management.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table'; // Import MatTableModule
+import { MatTableDataSource } from '@angular/material/table';
+import { CommonModule } from '@angular/common'; // Required for common directives like ngFor, ngIf
+import { WebUiService } from '../services/web-ui.service';
+import { IAsset } from '../models/iasset';
 @Component({
   selector: 'app-asset-management',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule 
+  ],
   templateUrl: './asset-management.component.html',
   styleUrl: './asset-management.component.css'
 })
-export class AssetManagementComponent {
+export class AssetManagementComponent implements OnInit {
+    displayedColumns: string[] = ['id', 'name', 'x', 'y', 'lastSync', 'floorMapId', 'active'];
+    dataSource = new MatTableDataSource<IAsset>([]);
 
+    constructor(private webUiService: WebUiService){}
+    
+    ngOnInit(): void{
+        this.fetchAssets();
+    }
+
+    fetchAssets(): void {
+      this.webUiService.getAssets().subscribe({
+        next: (data) => (this.dataSource.data = data),
+        error: (err) => console.error ('Error fetching data', err)
+      });
+    }
 }
