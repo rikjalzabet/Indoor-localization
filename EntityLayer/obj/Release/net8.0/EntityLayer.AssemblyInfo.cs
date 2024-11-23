2782dd96380a0f597028402bf3efd4dc95126869
David Škudar <dskudar20@foi.hr>
2024-11-23 22:58:56 +0100

    SCRUM-79 Created IAssetPositionHistory interface.

--- EntityLayer/obj/Release/net8.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Release/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd566d999064abd7af28ef4eaeeca5bc82f57fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc0889f2379b111711220cceeb663ccf00672df")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
