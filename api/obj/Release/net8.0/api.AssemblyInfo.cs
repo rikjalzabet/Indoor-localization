e0af9f7ee34e979dae85773635efed1c7a4a9137
David Škudar <dskudar20@foi.hr>
2024-11-23 22:50:11 +0100

    SCRUM-53

--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fb92d7b00c42e407759235a1fba7134b8f6df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce69c271c1de3077b53559868cd6242d94d0bff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
