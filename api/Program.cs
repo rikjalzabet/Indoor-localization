b1291f411d239bb4359bceda8b881ba3aea38c29
David Škudar <dskudar20@foi.hr>
2024-11-24 09:51:34 +0100

    SCRUM-53 In program.cs added two lines of code (builder.Service.AddSingleton).

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,6 @@
+using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// zamijeniti sa AddScoped nakon kreiranja EFC-a
+
+builder.Services.AddSingleton<IAssetRepository, MockAssetRepository>();
+builder.Services.AddSingleton<IAssetService, AssetService>();
 
 var app = builder.Build();
 
--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb26ef289a2cc92f18b6e46989b1afa802fd8cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a05b09f86d903936b192ecb56f517a1ec346f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
