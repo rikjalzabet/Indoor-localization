73e2cef20f3ff94f085533d4fbc173eb0dca1133
David Škudar <dskudar20@foi.hr>
2024-11-24 09:24:14 +0100

    SCRUM-53 Created BusinessLogicLayer project.

---
+++ BusinessLogicLayer/IAssetService.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Entities;
+
+namespace BusinessLogicLayer
+{
+    public interface IAssetService
+    {
+        List<Asset> GetAllAssets();
+        Asset GetAssetById(int id);
+        bool AddAsset(Asset asset);
+        bool UpdateAsset(Asset asset);
+        bool DeleteAsset(int id);
+    }
+}
