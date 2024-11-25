461e40432418076b610fe15442f302380d4d9e20
Antonijo Hamzić <ahamzic20@student.foi.hr>
2024-11-25 23:37:46 +0100

    SCRUM-170 Added sorting and filtering

--- web-ui/src/app/asset-management/asset-management.component.ts
+++ web-ui/src/app/asset-management/asset-management.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableModule } from '@angular/material/table'; // Import MatTableModule
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common'; // Required for common directives like ngFor, ngIf
 import { WebUiService } from '../services/web-ui.service';
 import { IAsset } from '../models/iasset';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-asset-management',
   standalone: true,
   imports: [
     CommonModule,
-    MatTableModule 
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule
   ],
   templateUrl: './asset-management.component.html',
   styleUrl: './asset-management.component.css'
@@ -18,11 +24,24 @@ export class AssetManagementComponent implements OnInit {
     displayedColumns: string[] = ['id', 'name', 'x', 'y', 'lastSync', 'floorMapId', 'active'];
     dataSource = new MatTableDataSource<IAsset>([]);
 
+    @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+
     constructor(private webUiService: WebUiService){}
     
     ngOnInit(): void{
         this.fetchAssets();
     }
+
+    ngAfterViewInit() {
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
     fetchAssets(): void {
       this.webUiService.getAssets().subscribe({
