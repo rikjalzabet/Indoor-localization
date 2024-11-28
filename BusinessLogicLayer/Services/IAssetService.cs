590efff54d1922a3518a258c6a791fd27df39ae1
David Škudar <dskudar20@foi.hr>
2024-11-28 09:28:06 +0100

    SCRUM-84 Updated IAssetService interface.

--- BusinessLogicLayer/Services/IAssetService.cs
+++ BusinessLogicLayer/Services/IAssetService.cs
@@ -5,7 +5,7 @@ namespace BusinessLogicLayer.Services
     public interface IAssetService
     {
         Task<List<Asset>> GetAllAssets();
-        Asset GetAssetById(int id);
+        Task<Asset> GetAssetById(int id);
         Task<bool> AddAsset(Asset asset);
         bool UpdateAsset(Asset asset);
         bool DeleteAsset(int id);
