de60a021c0b514580438b0cd72cf7fc3bd919a5c
David Škudar <dskudar20@foi.hr>
2024-11-28 09:24:21 +0100

    SCRUM-84 Updated IAssetRepository.

--- DataAccessLayer/Repositories/IAssetRepository.cs
+++ DataAccessLayer/Repositories/IAssetRepository.cs
@@ -10,7 +10,7 @@ namespace DataAccessLayer.Repositories
     public interface IAssetRepository
     {
         Task<List<Asset>> GetAllAssets();
-        Asset GetAssetById(int id);
+        Task<Asset> GetAssetById(int id);
         Task<int> AddAsset(Asset asset);
         int UpdateAsset(Asset asset);
         int DeleteAsset(int id);
