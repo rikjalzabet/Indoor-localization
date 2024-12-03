85d4e118df2b3125babd019ba09d79ea1bd8c351
David Škudar <dskudar20@foi.hr>
2024-12-03 11:06:36 +0100

    SCRUM-81 Created entity class Zone.

--- EntityLayer/Entities/Zone.cs
+++ EntityLayer/Entities/Zone.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Entities
 {
-    public interface IZone
+    public class Zone : IZone
     {
         public int Id { get; set; }
         public string Name { get; set; }
