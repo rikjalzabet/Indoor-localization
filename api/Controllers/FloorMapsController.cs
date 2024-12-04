3e8b793e0ad81386981a76e5989616f679bd4eb9
David Škudar <dskudar20@foi.hr>
2024-12-04 22:34:34 +0100

    SCRUM-87 Implemented function for adding floormap in FloorMapsController.

---
+++ api/Controllers/FloorMapsController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogicLayer.Services;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FloorMapsController : ControllerBase
+    {
+        private readonly IFloorMapService _floorMapService;
+        public FloorMapsController(IFloorMapService floorMapService)
+        {
+            _floorMapService = floorMapService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] FloorMap floorMap)
+        {
+            bool isAdded = false;
+            if (floorMap != null)
+            {
+                isAdded = await _floorMapService.AddFloorMap(floorMap);
+            }
+            if (isAdded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
