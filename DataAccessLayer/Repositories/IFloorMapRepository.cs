a5bafb1fce18962414b411e4bd9e22582da2c06a
David Škudar <dskudar20@foi.hr>
2024-12-04 22:17:15 +0100

    SCRUM-80 Created IFloorMapRepository interface.

---
+++ DataAccessLayer/Repositories/IFloorMapRepository.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IFloorMapRepository
+    {
+        Task<List<FloorMap>> GetAllFloorMaps();
+        Task<FloorMap> GetFloorMapById(int id);
+        Task<int> AddFloorMap(Asset asset);
+        Task<int> UpdateFloorMap(FloorMap floorMap);
+        Task<int> DeleteFloorMap(int id);
+    }
+}
