21122efb950594d39bae5544f15c46c6df788913
ahamzic20 <ahamzic20@foi.hr>
2024-12-06 21:24:22 +0100

    SCRUM-191 Created navigation

--- web-ui/src/app/app.routes.ts
+++ web-ui/src/app/app.routes.ts
@@ -1,7 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AssetManagementComponent } from './asset-management/asset-management.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'asset-managment', pathMatch: 'full'},
     { path: 'asset-managment', component: AssetManagementComponent },
+    { path: 'reports', component: ReportsComponent },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
