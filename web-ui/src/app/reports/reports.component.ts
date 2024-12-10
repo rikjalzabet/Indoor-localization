efd83e3fdb6d8141d3c46f9dc9211d00477687c4
ahamzic20 <ahamzic20@foi.hr>
2024-12-10 17:45:16 +0100

    SCRUM-193 Created table with asset history

--- web-ui/src/app/reports/reports.component.ts
+++ web-ui/src/app/reports/reports.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableModule,MatTableDataSource } from '@angular/material/table';
 import { WebUiService } from '../services/web-ui.service';
 import { IFloorMap } from '../models/IFloorMap';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,27 +10,58 @@ import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { IAssetPositionHistory } from '../models/IAssetPositionHistory';
-
+import {MatCardModule} from '@angular/material/card';
+import { IAsset } from '../models/iasset';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatRadioModule } from '@angular/material/radio';
 @Component({
   selector: 'app-reports',
   standalone: true,
   imports: [ MatFormFieldModule,
     MatSelectModule,
     FormsModule,
-  CommonModule,],
+  CommonModule,
+  MatCardModule,
+  MatIconModule,
+  MatSortModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatRadioModule
+  ,],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent implements OnInit{
   floorMaps? : IFloorMap[];
+  assets? : IAsset[];
+  dataSource = new MatTableDataSource<IAssetPositionHistory>([]);
+  displayedColumns: string[] = ['asset', 'x', 'y', 'dateTime'];
   floorMapMap = new Map<number, string>();
   selectedFloorMapId?: number;
   assetPositionHistory? : IAssetPositionHistory[];
+  assetMap = new Map<number,string>();
+  selection = new SelectionModel<IAsset>(false, []);
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private webUiService: WebUiService){}
 
   ngOnInit(): void {
     this.fetchFloorMaps();
   }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+}
+
+
+applyFilter(event: Event): void {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+
 
   fetchFloorMaps(): void {
     this.webUiService.getFloorMaps().subscribe({
@@ -38,12 +73,26 @@ export class ReportsComponent implements OnInit{
     });
   }
 
+  fetchAssets(): void {
+    this.webUiService.getAssets().subscribe({
+      next: (data) => {this.assets = data;
+        this.assets.forEach(ass => this.assetMap.set(ass.id,ass.name));
+      },
+      error: (err) => console.error ('Error fetching data', err)
+    });
+  }
+
+  getAssetName(assetId: number): string {
+    return this.assetMap.get(assetId) || 'Unknown' ;
+  }
+
   getFloorMapImage(floorMapId?: number): string | undefined {
     if (floorMapId === undefined) {
       return undefined;
     }
     const floorMap = this.floorMaps?.find((fm) => fm.id === floorMapId);
     this.fetchAssetPositionHistory(floorMapId);
+    this.fetchAssets();
     return floorMap?.image;
   }
 
@@ -51,6 +100,7 @@ export class ReportsComponent implements OnInit{
     this.webUiService.getAssetPositionHistory(floorMapId).subscribe({
       next: (data) => {
         this.assetPositionHistory = data;
+        this.dataSource.data = data;
       },
       error: (err) => console.error('Error fetching data', err)
     });
