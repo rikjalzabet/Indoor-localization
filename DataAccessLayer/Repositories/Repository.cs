1b1e5840ba0451759aaf7f9f2dd2e309fa5815e4
David Škudar <dskudar20@foi.hr>
2024-12-18 17:21:09 +0100

    SCRUM-140 Implemented Repository class.

--- DataAccessLayer/Repositories/Repository.cs
+++ DataAccessLayer/Repositories/Repository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly AppDbContext _context;
+        public DbSet<T> Entities;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            Entities = _context.Set<T>();
+        }
+        public async Task<int> AddAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            await Entities.AddAsync(entity);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity == null)
+                throw new KeyNotFoundException($"Entity with ID {id} not found.");
+
+            Entities.Attach(entity);
+            Entities.Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await Entities.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await Entities.FindAsync(id);
+            if (entity == null)
+                throw new KeyNotFoundException($"Entity with ID {id} not found.");
+
+            return entity;
+        }
+
     }
 }
