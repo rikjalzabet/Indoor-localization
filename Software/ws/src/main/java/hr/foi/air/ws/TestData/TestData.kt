4f264ebcc59fcc044febe39b75e1acca8eac9d2e
Rikardo Jalzabetic <rjalzabet20@student.foi.hr>
2024-12-22 14:51:45 +0100

    Refactoring - ws normal testData in comments

--- Software/ws/src/main/java/hr/foi/air/ws/TestData/TestData.kt
+++ Software/ws/src/main/java/hr/foi/air/ws/TestData/TestData.kt
@@ -10,47 +10,48 @@ import hr.foi.air.core.models.impl.Zone
 import hr.foi.air.core.parser.zonesList
 import java.util.Date
 
+
 class TestData {
-    companion object {
-        fun getAssets(): List<IAsset> {
-            return listOf(
-                Asset(1, "IAsset 1", 10.5f, 20.3f, Date(), 1, true),
-                Asset(2, "IAsset 2", 15.7f, 25.8f, Date(), 1, false),
-                Asset(3, "IAsset 3", 20.0f, 30.0f, Date(), 2, true)
-            )
-        }
-
-        fun getFloorMaps(): List<IFloorMap> {
-            return listOf(
-                FloorMap(1, "Floor 1 (local)", "local_floor_map_test_1"),
-                FloorMap(2, "Floor 2 (online)", "https://cdn.sick.com/media/content/he4/h01/10666047406110.jpg"),
-            )
-        }
-
-        fun getZones(): List<IZone> {
+    /*    companion object {
+            fun getAssets(): List<IAsset> {
                 return listOf(
-                    /* Zone(
-                         2, "IZone 1", listOf(
-                             Point(0.2f, 0.2f, 1),
-                             Point(0.4f, 0.4f, 2),
-                             Point(0.2f, 0.4f, 3),
-                             Point(0.4f, 0.2f, 4)
-                         )
-                     ),*/
-                    Zone(
-                        3, "IZone 2", listOf(
-                            Point(0.1f, 0.1f, 1),
-                            Point(0.5f, 0.1f, 2),
-                            Point(0.5f, 0.5f, 3),
-                            Point(0.1f, 0.5f, 4)
-                        )
-                    )
+                    Asset(1, "IAsset 1", 10.5f, 20.3f, Date(), 1, true),
+                    Asset(2, "IAsset 2", 15.7f, 25.8f, Date(), 1, false),
+                    Asset(3, "IAsset 3", 20.0f, 30.0f, Date(), 2, true)
                 )
             }
-        fun addZonesToList(){
-            for (zone in getZones()){
-                zonesList.add(zone)
+
+            fun getFloorMaps(): List<IFloorMap> {
+                return listOf(
+                    FloorMap(1, "Floor 1 (local)", "local_floor_map_test_1"),
+                    FloorMap(2, "Floor 2 (online)", "https://cdn.sick.com/media/content/he4/h01/10666047406110.jpg"),
+                )
             }
-        }
-    }
+
+            fun getZones(): List<IZone> {
+                    return listOf(
+                        /* Zone(
+                             2, "IZone 1", listOf(
+                                 Point(0.2f, 0.2f, 1),
+                                 Point(0.4f, 0.4f, 2),
+                                 Point(0.2f, 0.4f, 3),
+                                 Point(0.4f, 0.2f, 4)
+                             )
+                         ),*/
+                        Zone(
+                            3, "IZone 2", listOf(
+                                Point(0.1f, 0.1f, 1),
+                                Point(0.5f, 0.1f, 2),
+                                Point(0.5f, 0.5f, 3),
+                                Point(0.1f, 0.5f, 4)
+                            )
+                        )
+                    )
+                }
+            fun addZonesToList(){
+                for (zone in getZones()){
+                    zonesList.add(zone)
+                }
+            }
+        }*/
 }
