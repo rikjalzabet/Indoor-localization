fd8474b563332462c751b60a5f34474c7f7a6c9e
David Škudar <dskudar20@foi.hr>
2024-12-27 21:32:06 +0100

    SCRUM-95 Created AssetZoneHistoryRepository interface and class.

---
+++ DataAccessLayer/Repositories/AssetZoneHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AssetZoneHistoryRepository : Repository<AssetZoneHistory>, IAssetZoneHistoryRepository
+    {
+        public AssetZoneHistoryRepository(AppDbContext context) : base(context)
+        {
+
+        }
+
+    }
+}
