7e51e3387d2558c57ef6d7c3385277ba2bd0acab
David Škudar <dskudar20@foi.hr>
2024-12-27 21:22:46 +0100

    SCRUM-95 Created AssetZoneHistory entity class.

---
+++ EntityLayer/Entities/AssetZoneHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class AssetZoneHistory
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public int ZoneId { get; set; }
+        public DateTime EnterDateTime { get; set; }
+        public DateTime? ExitDateTime { get; set; }
+        public TimeSpan? RetentionTime { get; set; }
+
+        public virtual Asset Asset { get; set; }
+        public virtual Zone Zone { get; set; }
+    }
+}
