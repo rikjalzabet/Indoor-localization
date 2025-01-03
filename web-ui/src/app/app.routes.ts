9aa2f2fd9f78ada802ba7cb1bda549b0387e489c
ahamzic20 <ahamzic20@foi.hr>
2025-01-03 21:50:51 +0100

    SCRUM-27 Crated dashboard page and list of all avalible floormaps

--- web-ui/src/app/app.routes.ts
+++ web-ui/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AssetManagementComponent } from './asset-management/asset-management.component';
 import { ReportsComponent } from './reports/reports.component';
 import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'asset-managment', pathMatch: 'full'},
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     { path: 'asset-managment', component: AssetManagementComponent },
     { path: 'reports', component: ReportsComponent },
+    { path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
---
+++ web-ui/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { IFloorMap } from '../models/IFloorMap';
+import { WebUiService } from '../services/web-ui.service';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule,
+    MatButtonModule,
+    MatListModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+  floorMaps? : IFloorMap[];
+  floorMapMap = new Map<number, string>();
+  selectedFloorMapId?: number;
+
+  constructor(private webUiService: WebUiService){}
+  
+  ngOnInit(): void {
+    this.fetchFloorMaps();
+  }
+
+  fetchFloorMaps(): void {
+    this.webUiService.getFloorMaps().subscribe({
+      next: (data) => {
+        this.floorMaps = data;
+        this.floorMaps.forEach(fm => this.floorMapMap.set(fm.id, fm.name));
+      },
+      error: (err) => console.error('Error fetching data', err)
+    });
+  }
+
+  getFloorMapImage(floorMapId?: number): string | undefined {
+    if (floorMapId === undefined) {
+      return undefined;
+    }
+    const floorMap = this.floorMaps?.find((fm) => fm.id === floorMapId);
+    return floorMap?.image;
+  }
+}
