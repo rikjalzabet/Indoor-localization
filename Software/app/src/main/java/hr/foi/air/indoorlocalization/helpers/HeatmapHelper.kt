76bcf669d612f4372e1e5caaffa4b4d816063e42
angrychill <93102183+angrychill@users.noreply.github.com>
2025-01-05 15:15:44 +0100

    Added function for calculating heatmap within date range to HeatmapHelper class

--- Software/app/src/main/java/hr/foi/air/indoorlocalization/helpers/HeatmapHelper.kt
+++ Software/app/src/main/java/hr/foi/air/indoorlocalization/helpers/HeatmapHelper.kt
@@ -2,6 +2,7 @@ package hr.foi.air.indoorlocalization.helpers
 
 import android.icu.text.DateFormat
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import hr.foi.air.core.models.HeatmapDot
 import hr.foi.air.core.models.impl.Asset
@@ -9,6 +10,7 @@ import hr.foi.air.core.models.impl.AssetPositionHistory
 import hr.foi.air.core.parser.historyAssetPositionList
 import hr.foi.air.core.parser.liveAssetPositionList
 import java.util.Date
+import kotlin.math.max
 
 
 fun calculateColorForFrequency(frequency: Int): Color {
@@ -29,22 +31,35 @@ fun calculateSizeForColor(color: Color, dot: HeatmapDot): Float {
         Color.Blue -> dot.maxSize
         Color.Green -> dot.maxSize-10
         Color.Yellow -> dot.minSize
-        else -> dot.liveMovementSize
+        else -> dot.maxSize
     }
 }
 
-fun calculateDotFrequency() {
-    // TODO
-}
 
-fun groupAssetPositionHistoryByTime(data: List<AssetPositionHistory>): Map<Date, List<AssetPositionHistory>>{
-    return data.groupBy { it.dateTime }
-}
-
-fun convertAssetPositionHistoryToHeatmapPoints(floorMapId : Int, data: List<AssetPositionHistory>) : List<HeatmapDot>{
+fun calculateHeatmapDotsInDateRange(floorMapId : Int, size : Size, data : List<AssetPositionHistory>, from_date: Date, to_date: Date,
+                                    maxFrequency : Int) : List<HeatmapDot> {
 
     val historyAssetPositions = historyAssetPositionList.filter { it.floorMapId == floorMapId }
+    val historyWithinRange = historyAssetPositions.filter { it.dateTime in from_date..to_date}
+    val groupedHistory = historyWithinRange.groupBy {
+        Offset(
+            x = (it.x / size.width),
+            y = (it.y / size.height)
+        )
+    }
 
-   // TODO
+    val heatmapDots = groupedHistory.map{ (position, items) ->
+        val frequency = items.size
+        val alpha = (frequency.toFloat() / maxFrequency).coerceIn(0.2f, 1.0f)
+        val dotSize = (frequency.toFloat() / maxFrequency).coerceIn(15f, 100f)
 
+        HeatmapDot(
+            position = position,
+            frequency = frequency,
+            alpha = alpha,
+            size = dotSize
+        )
+    }
+
+    return heatmapDots
 }
