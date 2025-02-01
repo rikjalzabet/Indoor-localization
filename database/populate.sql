d3c8f7d0735267089b5dae0d50943505843dd183
Jana Jambrešić <jjambresi21@student.foi.hr>
2025-02-01 14:25:58 +0100

    SCRUM-143 Update data and image links for floor maps

--- database/populate.sql
+++ database/populate.sql
@@ -1,18 +1,15 @@
 -- Generate some floorMaps records
 INSERT INTO floorMaps (name, image) VALUES
-('FloorMap944', 'https://picsum.photos/id/944/500/500'),
-('FloorMap343', 'https://picsum.photos/id/343/500/500'),
-('FloorMap69', 'https://picsum.photos/id/69/500/500'),
-('FloorMap879', 'https://picsum.photos/id/879/500/500'),
-('FloorMap333', 'https://picsum.photos/id/333/500/500');
+('FloorMap1', 'https://wpmedia.roomsketcher.com/content/uploads/2022/01/06145940/What-is-a-floor-plan-with-dimensions.png'),
+('FloorMap2', 'https://wpmedia.roomsketcher.com/content/uploads/2022/01/06150346/2-Bedroom-Home-Plan-With-Dimensions.png');
 
 -- Generate some assets records
 INSERT INTO assets (name, x, y, lastSync, floorMapId, active) VALUES
 ('Forklift_001', 15.345678, 25.987654, NOW() - INTERVAL '5 seconds', 1, TRUE),
-('Palette_102', 30.654321, 40.123456, NOW() - INTERVAL '10 seconds', 2, TRUE),
-('Box_345', 5.123456, 10.987654, NOW() - INTERVAL '15 seconds', 3, TRUE),
-('Device_901', 50.765432, 60.876543, NOW() - INTERVAL '20 seconds', 4, TRUE),
-('Palette_203', 70.543210, 80.678901, NOW() - INTERVAL '25 seconds', 5, TRUE);
+('Palette_102', 30.654321, 40.123456, NOW() - INTERVAL '10 seconds', 2, FALSE),
+('Box_345', 5.123456, 10.987654, NOW() - INTERVAL '15 seconds', 1, FALSE),
+('Device_901', 50.765432, 60.876543, NOW() - INTERVAL '20 seconds', 2, TRUE),
+('Palette_203', 70.543210, 80.678901, NOW() - INTERVAL '25 seconds', 1, TRUE);
 
 -- Generate some zones records
 INSERT into zones (name, points) VALUES
@@ -26,14 +23,14 @@ INSERT into zones (name, points) VALUES
 INSERT INTO assetPositionHistory (assetId, x, y, dateTime, floorMapId) VALUES
 (1, 15.456789, 26.123456, NOW() - INTERVAL '1 minute', 1),
 (2, 30.987654, 40.654321, NOW() - INTERVAL '2 minutes', 2),
-(3, 5.543210, 11.234567, NOW() - INTERVAL '3 minutes', 3),
-(4, 50.876543, 61.123456, NOW() - INTERVAL '4 minutes', 4),
-(5, 70.654321, 81.345678, NOW() - INTERVAL '5 minutes', 5),
+(3, 5.543210, 11.234567, NOW() - INTERVAL '3 minutes', 1),
+(4, 50.876543, 61.123456, NOW() - INTERVAL '4 minutes', 2),
+(5, 70.654321, 81.345678, NOW() - INTERVAL '5 minutes', 1),
 (1, 16.123456, 27.987654, NOW() - INTERVAL '6 minutes', 1),
 (2, 31.654321, 41.123456, NOW() - INTERVAL '7 minutes', 2),
-(3, 6.234567, 12.876543, NOW() - INTERVAL '8 minutes', 3),
-(4, 51.543210, 62.234567, NOW() - INTERVAL '9 minutes', 4),
-(5, 71.345678, 82.456789, NOW() - INTERVAL '10 minutes', 5);
+(3, 6.234567, 12.876543, NOW() - INTERVAL '8 minutes', 1),
+(4, 51.543210, 62.234567, NOW() - INTERVAL '9 minutes', 2),
+(5, 71.345678, 82.456789, NOW() - INTERVAL '10 minutes', 1);
 
 -- Generate some assetZoneHistory records
 INSERT INTO assetZoneHistory (assetId, zoneId, enterDateTime, exitDateTime, retentionTime) VALUES
