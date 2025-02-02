17fa02352c015cffa9144f68d5876e531ee460e4
David Škudar <dskudar20@foi.hr>
2025-02-02 09:41:21 +0100

    SCRUM-31 Reports are updated to connect to the API

--- web-ui/src/app/services/web-ui.service.ts
+++ web-ui/src/app/services/web-ui.service.ts
@@ -25,25 +25,7 @@ export class WebUiService {
     }
 
     public getZones(): Observable<IZones[]> {
-      const mockFloorMaps: IZones[] = [
-        {
-          id :1,
-          name: 'Zone A',
-          points: '1'
-        },
-        {
-          id :3,
-          name: 'Zone B',
-          points: '1'
-        },
-        {
-          id :3,
-          name: 'Zone C',
-          points: '1'
-        }
-      ];
-
-      return of(mockFloorMaps);
+      return this.http.get<IZones[]>(`${this.apiUrl}/zones`);
     }
 
     public deleteAsset(Id: number): void {
@@ -85,71 +67,10 @@ export class WebUiService {
 
     public getAssetPositionHistory(FloorMapId: number): Observable<IAssetPositionHistory[]>  {
       return this.http.get<IAssetPositionHistory[]>(`${this.apiUrl}/assetPositionHistory/${FloorMapId}`);
-      if(FloorMapId==1){
-      const mockAssetPositionHistory: IAssetPositionHistory[] = [
-        {
-          id: 1,
-          assetId: 1,
-          x: 100,
-          y: 100,
-          dateTime: new Date('2024-12-09T10:00:00Z'),
-          floorMapId: 1,
-      },
-      {
-          id: 2,
-          assetId: 1,
-          x: 200,
-          y: 150,
-          dateTime: new Date('2024-12-09T10:05:00Z'),
-          floorMapId: 1,
-      },
-      {
-          id: 3,
-          assetId: 2,
-          x: 50.34,
-          y: 90.78,
-          dateTime: new Date('2024-12-09T10:10:00Z'),
-          floorMapId: 1,
-      }
-      ];
-
-      return of(mockAssetPositionHistory);
-    }else{
-      const mockAssetPositionHistory: IAssetPositionHistory[] = [
-        {
-          id: 6,
-          assetId: 2,
-          x: 100,
-          y: 100,
-          dateTime: new Date('2024-12-09T10:00:00Z'),
-          floorMapId: 1,
-      },
-      ];
-
-      return of(mockAssetPositionHistory);
-    }}
+    }
 
     public getAssetZoneHistory(): Observable<IAssetZoneHistory[]>  {
-      const mockAssetPositionHistory: IAssetZoneHistory[] = [
-        {
-          id: 1,
-          assetId: 1,
-          zoneId : 1,
-        enterDateTime :new Date('2024-12-09T10:00:00Z'),
-          exitDateTime: new Date('2024-12-09T10:00:00Z'),
-          retentionTime: 1,
-      },
-      {
-        id: 2,
-        assetId: 1,
-        zoneId : 1,
-      enterDateTime :new Date('2024-12-09T10:00:00Z'),
-        exitDateTime: new Date('2024-12-09T10:00:00Z'),
-        retentionTime: 1,
-      },
-      ];
-
-      return of(mockAssetPositionHistory);
+      return this.http.get<IAssetZoneHistory[]>(`${this.apiUrl}/assetZoneHistory`);
     }
   }
 
